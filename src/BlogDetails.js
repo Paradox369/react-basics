c0679abc90e02666daf0b017914951f4d6e24131
Paradox369 <george.ketcha@gmail.com>
2023-03-01 02:35:18 +0100

    final touches

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,25 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const {
+    data: blog,
+    isLoading,
+    error,
+  } = useFetch(`http://localhost:8000/blogs/${id}`);
 
   return (
     <div className="blog-details">
-      <h2>blog details - {id}</h2>
+      {isLoading && <div>loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title} </h2>
+          <p>written by {blog.author} </p>
+          <div>{blog.body} </div>
+        </article>
+      )}
     </div>
   );
 };
